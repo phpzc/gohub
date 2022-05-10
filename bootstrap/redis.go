501badc0a91f42300bc52c431860a99339370f90
phpzc <zhang5474jj@163.com>
2022-05-10 18:18:27 +0800

    使用 Redis

---
+++ bootstrap/redis.go
@@ -0,0 +1,18 @@
+package bootstrap
+
+import (
+	"fmt"
+	"gohub/pkg/config"
+	"gohub/pkg/redis"
+)
+
+func SetupRedis() {
+
+	//建立redis连接
+	redis.ConnectRedis(
+		fmt.Sprintf("%v:%v", config.GetString("redis.host"), config.GetString("redis.port")),
+		config.GetString("redis.username"),
+		config.GetString("redis.password"),
+		config.GetInt("redis.database"),
+	)
+}
