b13de861e51f95c4871695d80554178e41161581
phpzc <zhang5474jj@163.com>
2022-05-16 19:55:43 +0800

    添加发送手机验证码

--- pkg/verifycode/verifycode.go
+++ pkg/verifycode/verifycode.go
@@ -7,6 +7,7 @@ import (
 	"gohub/pkg/helpers"
 	"gohub/pkg/mail"
 	"gohub/pkg/redis"
+	"gohub/pkg/sms"
 	"strings"
 	"sync"
 )
@@ -47,6 +48,25 @@ func (vc *VerifyCode) generateVerifyCode(key string) string {
 
 	return code
 
+}
+
+// SendSMS 发送短信验证码，调用示例：
+//         verifycode.NewVerifyCode().SendSMS(request.Phone)
+func (vc *VerifyCode) SendSMS(phone string) bool {
+
+	// 生成验证码
+	code := vc.generateVerifyCode(phone)
+
+	// 方便本地和 API 自动测试
+	if !app.IsProduction() && strings.HasPrefix(phone, config.GetString("verifycode.debug_phone_prefix")) {
+		return true
+	}
+
+	// 发送短信
+	return sms.NewSMS().Send(phone, sms.Message{
+		Template: config.GetString("sms.aliyun.template_code"),
+		Data:     map[string]string{"code": code},
+	})
 }
 
 //SendEmail 发送邮件验证码
