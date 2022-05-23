d2558ed35d4efff07b9783ffcae070fc29018d5a
phpzc <zhang5474jj@163.com>
2022-05-23 17:12:54 +0800

    JWT 授权

--- app/models/model.go
+++ app/models/model.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/spf13/cast"
+)
 
 type BaseModel struct {
 	ID uint64 `gorm:"column:id;primaryKey;autoIncrement;" json:"id,omitempty"`
@@ -10,4 +14,8 @@ type BaseModel struct {
 type CommonTimeStampsField struct {
 	CreatedAt time.Time `gorm:"column:created_at;index;" json:"created_at,omitempty"`
 	UpdatedAt time.Time `gorm:"column:updated_at;index;" json:"updated,omitempty"`
+}
+
+func (a BaseModel) GetStringID() string {
+	return cast.ToString(a.ID)
 }
