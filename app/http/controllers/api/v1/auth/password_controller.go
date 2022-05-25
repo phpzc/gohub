6eb6152147c012dda1be70f39c2d1a738c58f593
phpzc <zhang5474jj@163.com>
2022-05-25 15:42:53 +0800

    通过手机找回密码

---
+++ app/http/controllers/api/v1/auth/password_controller.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/models/user"
+	"gohub/app/requests"
+	"gohub/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+type PasswordController struct {
+	v1.BaseAPIController
+}
+
+//ResetByPhone 使用手机和验证码重置密码
+func (pc *PasswordController) ResetByPhone(c *gin.Context) {
+	//验证表单
+	request := requests.ResetByPhoneRequest{}
+
+	if ok := requests.Validate(c, &request, requests.ResetByPhone); !ok {
+		return
+	}
+
+	//更新密码
+	userModel := user.GetByPhone(request.Phone)
+	if userModel.ID == 0 {
+		response.Abort404(c)
+	} else {
+		userModel.Password = request.Password
+		userModel.Save()
+
+		response.Success(c)
+	}
+}
