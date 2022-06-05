a228d5a037c384017ed57ef76996cdda11aa5087
phpzc <zhang5474jj@163.com>
2022-06-05 18:01:44 +0800

    用户列表

--- routes/api.go
+++ routes/api.go
@@ -64,6 +64,9 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		uc := new(controller.UsersController)
 		//获取当前用户
 		v1.GET("/user", middlewares.AuthJWT(), uc.CurrentUser)
-
+		usersGroup := v1.Group("/users")
+		{
+			usersGroup.GET("", uc.Index)
+		}
 	}
 }
