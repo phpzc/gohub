5641eb21825d1100dc9d62293a90768a27ef45b6
phpzc <zhang5474jj@163.com>
2022-06-15 13:45:15 +0800

    图片裁切

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/disintegration/imaging"
 	"github.com/gin-gonic/gin"
 )
 
@@ -40,7 +41,7 @@ func SaveUploadAvatar(c *gin.Context, file *multipart.FileHeader) (string, error
 	var avatar string
 
 	publicPath := "public"
-	dirName := fmt.Sprintf("/uploads/avatars/%s/%s", app.TimenowInTimezone().Format("2006/01/02"), auth.CurrentUID(c))
+	dirName := fmt.Sprintf("/uploads/avatars/%s/%s/", app.TimenowInTimezone().Format("2006/01/02"), auth.CurrentUID(c))
 	os.MkdirAll(publicPath+dirName, 0755)
 
 	//保存文件
@@ -51,7 +52,28 @@ func SaveUploadAvatar(c *gin.Context, file *multipart.FileHeader) (string, error
 		return avatar, err
 	}
 
-	return avatarPath, nil
+	//裁切图片
+	img, err := imaging.Open(avatarPath, imaging.AutoOrientation(true))
+	if err != nil {
+		return avatar, err
+	}
+
+	resizeAvatar := imaging.Thumbnail(img, 256, 256, imaging.Lanczos)
+	resizeAvatarName := randomNameFromUploadFile(file)
+	resizeAvatarPath := publicPath + dirName + resizeAvatarName
+	err = imaging.Save(resizeAvatar, resizeAvatarPath)
+	if err != nil {
+		return avatar, err
+
+	}
+
+	//删除老文件
+	err = os.Remove(avatarPath)
+	if err != nil {
+		return avatar, err
+	}
+
+	return dirName + resizeAvatarName, nil
 }
 
 func randomNameFromUploadFile(file *multipart.FileHeader) string {
